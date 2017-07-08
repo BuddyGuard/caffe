cfca5d94df98a141fc1dcd61378790a2498cdd9f
karthik.govindappa <karthik.govindappa@buddyguard.io>
2017-07-08 22:32:50 +0200

    Update deep compression models scoring script

--- examples/ssd/score_pruned_models.py
+++ examples/ssd/score_pruned_models.py
@@ -1,4 +1,5 @@
 import os
+import re
 import numpy as np
 import subprocess
 
@@ -24,15 +25,204 @@ caffe_root = '/home/karthik/workspace/caffe'
 #cluster = True
 #decompressed = False
 
-# SSD VGGNet PASCAL LAYER INDEPENDENT PRUNED - RETRAINED 10K - CLUSTERED 256 - DECOMPRESSED
-decompressed_models_path = os.path.join(caffe_root, 'models/VGGNet/VOC0712/Layer_Independent_Pruned_Retrained_Clustered_Decompressed_Models')
-original_model_name = os.path.join(caffe_root, 'models/VGGNet/VOC0712/SSD_300x300/VGG_VOC0712_SSD_300x300_iter_60000.caffemodel')
-scoring_script = 'examples/ssd/score_pruned_ssd_pascal_vggnet.py'
-itr = 10000
+# SSD VGGNet - PASCAL - LAYER INDEPENDENT PRUNED - RETRAINED 15K - CLUSTERED 128
+#clustered_models_path = os.path.join(caffe_root, 'models/VGGNet/VOC0712/Layer_Independent_Pruned_Retrained_Clustered128_Models')
+#original_model_name = os.path.join(caffe_root, 'models/VGGNet/VOC0712/SSD_300x300/VGG_VOC0712_SSD_300x300_iter_60000.caffemodel')
+#scoring_script = 'examples/ssd/score_pruned_ssd_pascal_vggnet.py'
+#itr = 15000
+#prune_type = 'layer_indep'
+#prune = False
+#cluster = True
+#centroids = 128
+#decompressed = False
+
+# SSD VGGNet - PASCAL - LAYER INDEPENDENT PRUNED - RETRAINED 15K - CLUSTERED 64
+#clustered_models_path = os.path.join(caffe_root, 'models/VGGNet/VOC0712/Layer_Independent_Pruned_Retrained_Clustered64_Models')
+#original_model_name = os.path.join(caffe_root, 'models/VGGNet/VOC0712/SSD_300x300/VGG_VOC0712_SSD_300x300_iter_60000.caffemodel')
+#scoring_script = 'examples/ssd/score_pruned_ssd_pascal_vggnet.py'
+#itr = 15000
+#prune_type = 'layer_indep'
+#prune = False
+#cluster = True
+#centroids = 64
+#decompressed = False
+
+# SSD VGGNet - PASCAL - LAYER INDEPENDENT PRUNED - RETRAINED 15K - CLUSTERED 32
+#clustered_models_path = os.path.join(caffe_root, 'models/VGGNet/VOC0712/Layer_Independent_Pruned_Retrained_Clustered32_Models')
+#original_model_name = os.path.join(caffe_root, 'models/VGGNet/VOC0712/SSD_300x300/VGG_VOC0712_SSD_300x300_iter_60000.caffemodel')
+#scoring_script = 'examples/ssd/score_pruned_ssd_pascal_vggnet.py'
+#itr = 15000
+#prune_type = 'layer_indep'
+#prune = False
+#cluster = True
+#centroids = 32
+#decompressed = False
+
+# SSD VGGNet - PASCAL - LAYER INDEPENDENT PRUNED - RETRAINED 15K - CLUSTERED 16
+#clustered_models_path = os.path.join(caffe_root, 'models/VGGNet/VOC0712/Layer_Independent_Pruned_Retrained_Clustered16_Models')
+#original_model_name = os.path.join(caffe_root, 'models/VGGNet/VOC0712/SSD_300x300/VGG_VOC0712_SSD_300x300_iter_60000.caffemodel')
+#scoring_script = 'examples/ssd/score_pruned_ssd_pascal_vggnet.py'
+#itr = 15000
+#prune_type = 'layer_indep'
+#prune = False
+#cluster = True
+#centroids = 16
+#decompressed = False
+
+# SSD VGGNet - PASCAL - LAYER WISE PRUNED - RETRAINED 15K - CLUSTERED 128
+#clustered_models_path = os.path.join(caffe_root, 'models/VGGNet/VOC0712/Layer_Wise_Pruned_Retrained_Clustered128_Models')
+#original_model_name = os.path.join(caffe_root, 'models/VGGNet/VOC0712/SSD_300x300/VGG_VOC0712_SSD_300x300_iter_60000.caffemodel')
+#scoring_script = 'examples/ssd/score_pruned_ssd_pascal_vggnet.py'
+#itr = 15000
+#prune_type = 'layer_wise'
+#prune = False
+#cluster = True
+#centroids = 128
+#decompressed = False
+
+# SSD VGGNet - PASCAL - LAYER WISE PRUNED - RETRAINED 15K - CLUSTERED 64
+#clustered_models_path = os.path.join(caffe_root, 'models/VGGNet/VOC0712/Layer_Wise_Pruned_Retrained_Clustered64_Models')
+#original_model_name = os.path.join(caffe_root, 'models/VGGNet/VOC0712/SSD_300x300/VGG_VOC0712_SSD_300x300_iter_60000.caffemodel')
+#scoring_script = 'examples/ssd/score_pruned_ssd_pascal_vggnet.py'
+#itr = 15000
+#prune_type = 'layer_wise'
+#prune = False
+#cluster = True
+#centroids = 64
+#decompressed = False
+
+# SSD VGGNet - PASCAL - LAYER WISE PRUNED - RETRAINED 15K - CLUSTERED 32
+#clustered_models_path = os.path.join(caffe_root, 'models/VGGNet/VOC0712/Layer_Wise_Pruned_Retrained_Clustered32_Models')
+#original_model_name = os.path.join(caffe_root, 'models/VGGNet/VOC0712/SSD_300x300/VGG_VOC0712_SSD_300x300_iter_60000.caffemodel')
+#scoring_script = 'examples/ssd/score_pruned_ssd_pascal_vggnet.py'
+#itr = 15000
+#prune_type = 'layer_wise'
+#prune = False
+#cluster = True
+#centroids = 32
+#decompressed = False
+
+# SSD VGGNet - PASCAL - LAYER WISE PRUNED - RETRAINED 15K - CLUSTERED 16
+#clustered_models_path = os.path.join(caffe_root, 'models/VGGNet/VOC0712/Layer_Wise_Pruned_Retrained_Clustered16_Models')
+#original_model_name = os.path.join(caffe_root, 'models/VGGNet/VOC0712/SSD_300x300/VGG_VOC0712_SSD_300x300_iter_60000.caffemodel')
+#scoring_script = 'examples/ssd/score_pruned_ssd_pascal_vggnet.py'
+#itr = 15000
+#prune_type = 'layer_wise'
+#prune = False
+#cluster = True
+#centroids = 16
+#decompressed = False
+
+# SSD ResNet - PASCAL - LAYER WISE PRUNED - RETRAINED 15K - CLUSTERED 128
+#clustered_models_path = os.path.join(caffe_root, 'models/ResNet/VOC0712/Layer_Wise_Pruned_Retrained_Clustered128_Models')
+#original_model_name = os.path.join(caffe_root, 'models/ResNet/VOC0712/SSD_300x300/ResNet_VOC0712_SSD_300x300_iter_60000.caffemodel')
+#scoring_script = 'examples/ssd/score_pruned_ssd_pascal_resnet.py'
+#itr = 15000
+#prune_type = 'layer_wise'
+#prune = False
+#cluster = True
+#centroids = 128
+#decompressed = False
+
+# SSD ResNet - PASCAL - LAYER WISE PRUNED - RETRAINED 15K - CLUSTERED 64
+#clustered_models_path = os.path.join(caffe_root, 'models/ResNet/VOC0712/Layer_Wise_Pruned_Retrained_Clustered64_Models')
+#original_model_name = os.path.join(caffe_root, 'models/ResNet/VOC0712/SSD_300x300/ResNet_VOC0712_SSD_300x300_iter_60000.caffemodel')
+#scoring_script = 'examples/ssd/score_pruned_ssd_pascal_resnet.py'
+#itr = 15000
+#prune_type = 'layer_wise'
+#prune = False
+#cluster = True
+#centroids = 64
+#decompressed = False
+
+# SSD ResNet - PASCAL - LAYER WISE PRUNED - RETRAINED 15K - CLUSTERED 32
+#clustered_models_path = os.path.join(caffe_root, 'models/ResNet/VOC0712/Layer_Wise_Pruned_Retrained_Clustered32_Models')
+#original_model_name = os.path.join(caffe_root, 'models/ResNet/VOC0712/SSD_300x300/ResNet_VOC0712_SSD_300x300_iter_60000.caffemodel')
+#scoring_script = 'examples/ssd/score_pruned_ssd_pascal_resnet.py'
+#itr = 15000
+#prune_type = 'layer_wise'
+#prune = False
+#cluster = True
+#centroids = 32
+#decompressed = False
+
+# SSD ResNet - PASCAL - LAYER WISE PRUNED - RETRAINED 15K - CLUSTERED 16
+#clustered_models_path = os.path.join(caffe_root, 'models/ResNet/VOC0712/Layer_Wise_Pruned_Retrained_Clustered16_Models')
+#original_model_name = os.path.join(caffe_root, 'models/ResNet/VOC0712/SSD_300x300/ResNet_VOC0712_SSD_300x300_iter_60000.caffemodel')
+#scoring_script = 'examples/ssd/score_pruned_ssd_pascal_resnet.py'
+#itr = 15000
+#prune_type = 'layer_wise'
+#prune = False
+#cluster = True
+#centroids = 16
+#decompressed = False
+
+# SSD VGGNet - VOC0712CDP - LAYER INDEPENDENT PRUNED - RETRAINED 15K - CLUSTERED 256
+#clustered_models_path = os.path.join(caffe_root, 'models/VGGNet/VOC0712CDP/Layer_Independent_Pruned_Retrained_Clustered256_Models')
+#original_model_name = os.path.join(caffe_root, 'models/VGGNet/VOC0712CDP/SSD_300x300/VGG_VOC0712CDP_SSD_300x300_iter_60000.caffemodel')
+#scoring_script = 'examples/ssd/score_pruned_ssd_pascal_cdp_vggnet.py'
+#itr = 15000
+#prune_type = 'layer_indep'
+#prune = False
+#cluster = True
+#centroids = 256
+#decompressed = False
+
+# SSD VGGNet - VOC0712CDP - LAYER INDEPENDENT PRUNED - RETRAINED 15K - CLUSTERED 128
+#clustered_models_path = os.path.join(caffe_root, 'models/VGGNet/VOC0712CDP/Layer_Independent_Pruned_Retrained_Clustered128_Models')
+#original_model_name = os.path.join(caffe_root, 'models/VGGNet/VOC0712CDP/SSD_300x300/VGG_VOC0712CDP_SSD_300x300_iter_60000.caffemodel')
+#scoring_script = 'examples/ssd/score_pruned_ssd_pascal_cdp_vggnet.py'
+#itr = 15000
+#prune_type = 'layer_indep'
+#prune = False
+#cluster = True
+#centroids = 128
+#decompressed = False
+
+# SSD VGGNet - VOC0712CDP - LAYER INDEPENDENT PRUNED - RETRAINED 15K - CLUSTERED 64
+#clustered_models_path = os.path.join(caffe_root, 'models/VGGNet/VOC0712CDP/Layer_Independent_Pruned_Retrained_Clustered64_Models')
+#original_model_name = os.path.join(caffe_root, 'models/VGGNet/VOC0712CDP/SSD_300x300/VGG_VOC0712CDP_SSD_300x300_iter_60000.caffemodel')
+#scoring_script = 'examples/ssd/score_pruned_ssd_pascal_cdp_vggnet.py'
+#itr = 15000
+#prune_type = 'layer_indep'
+#prune = False
+#cluster = True
+#centroids = 64
+#decompressed = False
+
+# SSD VGGNet - VOC0712CDP - LAYER INDEPENDENT PRUNED - RETRAINED 15K - CLUSTERED 32
+#clustered_models_path = os.path.join(caffe_root, 'models/VGGNet/VOC0712CDP/Layer_Independent_Pruned_Retrained_Clustered32_Models')
+#original_model_name = os.path.join(caffe_root, 'models/VGGNet/VOC0712CDP/SSD_300x300/VGG_VOC0712CDP_SSD_300x300_iter_60000.caffemodel')
+#scoring_script = 'examples/ssd/score_pruned_ssd_pascal_cdp_vggnet.py'
+#itr = 15000
+#prune_type = 'layer_indep'
+#prune = False
+#cluster = True
+#centroids = 32
+#decompressed = False
+
+# SSD VGGNet - VOC0712CDP - LAYER INDEPENDENT PRUNED - RETRAINED 15K - CLUSTERED 16
+clustered_models_path = os.path.join(caffe_root, 'models/VGGNet/VOC0712CDP/Layer_Independent_Pruned_Retrained_Clustered16_Models')
+original_model_name = os.path.join(caffe_root, 'models/VGGNet/VOC0712CDP/SSD_300x300/VGG_VOC0712CDP_SSD_300x300_iter_60000.caffemodel')
+scoring_script = 'examples/ssd/score_pruned_ssd_pascal_cdp_vggnet.py'
+itr = 15000
 prune_type = 'layer_indep'
 prune = False
 cluster = True
-decompressed = True
+centroids = 16
+decompressed = False
+
+### DECOMPRESSED MODELS ###
+
+# SSD VGGNet PASCAL LAYER INDEPENDENT PRUNED - RETRAINED 10K - CLUSTERED 256 - DECOMPRESSED
+#decompressed_models_path = os.path.join(caffe_root, 'models/VGGNet/VOC0712/Layer_Independent_Pruned_Retrained_Clustered_Decompressed_Models')
+#original_model_name = os.path.join(caffe_root, 'models/VGGNet/VOC0712/SSD_300x300/VGG_VOC0712_SSD_300x300_iter_60000.caffemodel')
+#scoring_script = 'examples/ssd/score_pruned_ssd_pascal_vggnet.py'
+#itr = 10000
+#prune_type = 'layer_indep'
+#prune = False
+#cluster = True
+#decompressed = True
 
 # SSD VGGNet PASCAL - LAYER WISE PRUNED - RETRAINED 15K - CLUSTERED 256 - DECOMPRESSED
 #decompressed_models_path = os.path.join(caffe_root, 'models/VGGNet/VOC0712/Layer_Wise_Pruned_Retrained_Clustered_Models')
@@ -42,6 +232,28 @@ decompressed = True
 #itr = 15000
 #prune = False
 #cluster = True
+#decompressed = True
+
+# SSD - ResNet - PASCAL - LAYER WISE PRUNED - RETRAINED 15K - CLUSTERED 256 - DECOMPRESSED
+#decompressed_models_path = os.path.join(caffe_root, 'models/ResNet/VOC0712/Layer_Wise_Pruned_Retrained_Clustered256_Decompressed_Models')
+#original_model_name = os.path.join(caffe_root, 'models/ResNet/VOC0712/SSD_300x300/ResNet_VOC0712_SSD_300x300_iter_60000.caffemodel')
+#scoring_script = 'examples/ssd/score_pruned_ssd_pascal_resnet.py'
+#itr = 15000
+#prune_type = 'layer_wise'
+#prune = False
+#cluster = True
+#centroids = 256
+#decompressed = True
+
+# SSD VGGNet - VOC0712CDP - LAYER INDEPENDENT PRUNED - RETRAINED 15K - CLUSTERED 256 - DECOMPRESSED
+#decompressed_models_path = os.path.join(caffe_root, 'models/VGGNet/VOC0712CDP/Layer_Independent_Pruned_Retrained_Clustered256_Decompressed_Models')
+#original_model_name = os.path.join(caffe_root, 'models/VGGNet/VOC0712CDP/SSD_300x300/VGG_VOC0712CDP_SSD_300x300_iter_60000.caffemodel')
+#scoring_script = 'examples/ssd/score_pruned_ssd_pascal_cdp_vggnet.py'
+#itr = 15000
+#prune_type = 'layer_indep'
+#prune = False
+#cluster = True
+#centroids = 256
 #decompressed = True
 
 # SSD VGGNet COCO LAYER INDEP
@@ -90,14 +302,60 @@ decompressed = True
 #prune = False
 #cluster = True
 
-# SSD ResNet PASCAL LAYER WISE PRUNED - RETRAINED 15K - CLUSTERED 256
-#clustered_models_path = os.path.join(caffe_root, 'models/ResNet/VOC0712/Layer_Wise_Pruned_Retrained_Clustered_Models')
+# SSD ResNet - PASCAL - LAYER INDEPENDENT PRUNED - RETRAINED 15K - CLUSTERED 128
+#clustered_models_path = os.path.join(caffe_root, 'models/ResNet/VOC0712/Layer_Independent_Pruned_Retrained_Clustered128_Models')
 #original_model_name = os.path.join(caffe_root, 'models/ResNet/VOC0712/SSD_300x300/ResNet_VOC0712_SSD_300x300_iter_60000.caffemodel')
 #scoring_script = 'examples/ssd/score_pruned_ssd_pascal_resnet.py'
+#itr = 15000
+#prune_type = 'layer_indep'
+#prune = False
+#cluster = True
+#centroids = 128
+#decompressed = False
+
+# SSD ResNet - PASCAL - LAYER INDEPENDENT PRUNED - RETRAINED 15K - CLUSTERED 64
+#clustered_models_path = os.path.join(caffe_root, 'models/ResNet/VOC0712/Layer_Independent_Pruned_Retrained_Clustered64_Models')
+#original_model_name = os.path.join(caffe_root, 'models/ResNet/VOC0712/SSD_300x300/ResNet_VOC0712_SSD_300x300_iter_60000.caffemodel')
+#scoring_script = 'examples/ssd/score_pruned_ssd_pascal_resnet.py'
+#itr = 15000
+#prune_type = 'layer_indep'
+#prune = False
+#cluster = True
+#centroids = 64
+#decompressed = False
+
+# SSD ResNet - PASCAL - LAYER INDEPENDENT PRUNED - RETRAINED 15K - CLUSTERED 32
+#clustered_models_path = os.path.join(caffe_root, 'models/ResNet/VOC0712/Layer_Independent_Pruned_Retrained_Clustered32_Models')
+#original_model_name = os.path.join(caffe_root, 'models/ResNet/VOC0712/SSD_300x300/ResNet_VOC0712_SSD_300x300_iter_60000.caffemodel')
+#scoring_script = 'examples/ssd/score_pruned_ssd_pascal_resnet.py'
+#itr = 15000
+#prune_type = 'layer_indep'
+#prune = False
+#cluster = True
+#centroids = 32
+#decompressed = False
+
+# SSD ResNet - PASCAL - LAYER INDEPENDENT PRUNED - RETRAINED 15K - CLUSTERED 16
+#clustered_models_path = os.path.join(caffe_root, 'models/ResNet/VOC0712/Layer_Independent_Pruned_Retrained_Clustered16_Models')
+#original_model_name = os.path.join(caffe_root, 'models/ResNet/VOC0712/SSD_300x300/ResNet_VOC0712_SSD_300x300_iter_60000.caffemodel')
+#scoring_script = 'examples/ssd/score_pruned_ssd_pascal_resnet.py'
+#itr = 15000
+#prune_type = 'layer_indep'
+#prune = False
+#cluster = True
+#centroids = 16
+#decompressed = False
+
+# SSD ResNet PASCAL LAYER WISE PRUNED - RETRAINED 15K - CLUSTERED 256
+#clustered_models_path = os.path.join(caffe_root, 'models/ResNet/VOC0712/Layer_Wise_Pruned_Retrained_Clustered256_Models')
+#original_model_name = os.path.join(caffe_root, 'models/ResNet/VOC0712/SSD_300x300/ResNet_VOC0712_SSD_300x300_iter_60000.caffemodel')
+#scoring_script = 'examples/ssd/score_pruned_ssd_pascal_resnet.py'
+#itr = 15000
 #prune_type = 'layer_wise'
 #prune = False
 #cluster = True
-
+#centroids = 256
+#decompressed = False
 
 # SSD RESNet PASCAL - LAYER INDEPENDENT PRUNED - RETRAINED 15K - CLUSTERED 256 - DECOMPRESSED
 #decompressed_models_path = os.path.join(caffe_root, 'models/ResNet/VOC0712/Layer_Independent_Pruned_Retrained_Clustered_Decompressed_Models')
@@ -138,6 +396,7 @@ for model in models:
             base_idx = model.find(key_str)
             std_dev = model[base_idx-3:base_idx]
             prune_percent = model[base_idx+len(key_str):base_idx+len(key_str)+2] 
+            prune_percent = re.sub('\%$', '', prune_percent)
         # Copy pruned model to original model location
         if prune:
             caffemodel = os.path.join(pruned_models_path, model)
@@ -149,18 +408,18 @@ for model in models:
         print 'Copied {} -> {}'.format(caffemodel, original_model_name)
         # Run score script
         if prune and prune_type == 'layer_indep':
-            cmd = 'python {} {} {} {}'.format(scoring_script, 'prune', 'layer_indep', prune_percent)
+            cmd = 'python {} --pruned --pruned_type={} --prune_percent={}'.format(scoring_script, 'layer_indep', prune_percent)
         elif cluster and not decompressed and prune_type == 'layer_indep':
-            cmd = 'python {} {} {} {} {}'.format(scoring_script, 'cluster', 'layer_indep', prune_percent, ' ')
+            cmd = 'python {} --clustered --centroids={} --prune_type={} --prune_percent={}'.format(scoring_script,  centroids, 'layer_indep', prune_percent)
         elif cluster and decompressed and prune_type == 'layer_indep':
-            cmd = 'python {} {} {} {} {}'.format(scoring_script, 'cluster', 'layer_indep', prune_percent, 'decompressed')
+            cmd = 'python {} --clustered --centroids={} --prune_type={} --prune_percent={} --decompressed'.format(scoring_script, centroids, 'layer_indep', prune_percent)
         
-        elif prune and prune_type == 'layer_wise' and not decompressed:
-            cmd = 'python {} {} {} {} {} {}'.format(scoring_script, 'prune', 'layer_wise', prune_percent, std_dev, 'pruned')
-        elif cluster and not decompressed and prune_type == 'layer_wise':
-            cmd = 'python {} {} {} {} {} {}'.format(scoring_script, 'cluster', 'layer_wise', prune_percent, std_dev, ' ')
-        elif cluster and decompressed and prune_type == 'layer_wise':
-            cmd = 'python {} {} {} {} {} {}'.format(scoring_script, 'cluster', 'layer_wise', prune_percent, std_dev, 'decompressed')
+        elif prune and prune_type == 'layer_wise':
+            cmd = 'python {} --pruned --pruned_type={} --prune_percent{} --std_dev={}'.format(scoring_script, prune_type, prune_percent, std_dev)
+        elif cluster and not decompressed and prune_type=='layer_wise':
+            cmd = 'python {} --clustered --centroids={} --prune_type={} --prune_percent={} --std_dev={}'.format(scoring_script, centroids, prune_type, prune_percent, std_dev)
+        elif cluster and decompressed and prune_type=='layer_wise':
+            cmd = 'python {}  --clustered --decompressed --centroids={} --prune_type={} --prune_percent={} --std_dev={}'.format(scoring_script, centroids, prune_type, prune_percent, std_dev)
         print cmd
         subprocess.call(cmd, shell=True)
 
